e5a497c2415b4b089843c7217b9a7bdc08ab27fe
Geon <geonulim@gmail.com>
2024-01-23 18:06:28 +0900

    ✨ feat: 지도에서 주소에 따른 마커 표시 구현

--- app/(route)/event/[id]/_components/KakaoMap.tsx
+++ app/(route)/event/[id]/_components/KakaoMap.tsx
@@ -2,18 +2,41 @@
 
 import { useEffect } from "react";
 
-const KakaoMap = () => {
+interface Props {
+  name: string;
+  address: string;
+}
+
+const KakaoMap = ({ name, address }: Props) => {
   useEffect(() => {
     if (window.kakao) {
       window.kakao.maps.load(() => {
         const mapContainer = document.getElementById("map");
         const mapOption = {
-          // 초기 중심 좌표 설정
           center: new window.kakao.maps.LatLng(37.566826, 126.9786567),
-          // 줌 레벨 설정
           level: 3,
         };
-        new window.kakao.maps.Map(mapContainer, mapOption);
+        const map = new window.kakao.maps.Map(mapContainer, mapOption);
+
+        const geocoder = new window.kakao.maps.services.Geocoder();
+
+        geocoder.addressSearch(address, function (result: any, status: any) {
+          if (status === window.kakao.maps.services.Status.OK) {
+            const coords = new window.kakao.maps.LatLng(result[0].y, result[0].x);
+
+            const marker = new window.kakao.maps.Marker({
+              map: map,
+              position: coords,
+            });
+
+            const infowindow = new window.kakao.maps.InfoWindow({
+              content: `<div style="width:150px;text-align:center;padding:6px 0;">${name}</div>`,
+            });
+            infowindow.open(map, marker);
+
+            map.setCenter(coords);
+          }
+        });
       });
     }
   }, []);
--- app/(route)/event/[id]/_components/tabs/LocationTab.tsx
+++ app/(route)/event/[id]/_components/tabs/LocationTab.tsx
@@ -1,9 +1,11 @@
+"use client";
+
 import KakaoMap from "../KakaoMap";
 
 const LocationTab = () => {
   return (
     <div className="h-400 w-full">
-      <KakaoMap />
+      <KakaoMap name="을지로 위워크" address="서울 중구 삼일대로 343" />
     </div>
   );
 };
