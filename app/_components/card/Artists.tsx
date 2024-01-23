144bab36c5217647d4c21203ddfd79da0bede5ab
김나은 <nicolekim0115@gmail.com>
2024-01-23 16:15:27 +0900

    ⚡️ fix: 무한스크롤 조금 수정

--- app/_components/card/Artists.tsx
+++ app/_components/card/Artists.tsx
@@ -6,17 +6,23 @@ import ArtistProfile from "./ArtistProfile";
 import { MockData } from "./ProfileMockData";
 
 const Artists = () => {
-  // 무한스크롤 수정해야하는 부분
   const [artists, setArtists] = useState(MockData);
   const [hasMore, setHasMore] = useState(true);
 
+  // 이부분 바꿔야함 일단 무한스크롤 잘 되는지 테스트용..
   const loadMore = () => {
     const newArtists = [...artists, ...MockData];
-    setArtists(newArtists);
 
-    if (newArtists.length >= MockData.length) {
+    // 데이터가 9개 이상이면 무한스크롤 계속 진행
+    if (newArtists.length >= 9) {
+      setHasMore(true);
+    } else {
+      // 9개 미만이면 더 이상 무한스크롤 하지 않음
       setHasMore(false);
     }
+
+    // 새로운 아티스트 데이터를 적용
+    setArtists(newArtists);
   };
 
   return (
@@ -33,7 +39,7 @@ const Artists = () => {
         useWindow={false}
       >
         <ul className="grid w-320 grid-cols-3 gap-24 px-16">
-          {MockData.map((artist, index) => (
+          {artists.map((artist, index) => (
             <li key={index}>
               <ArtistProfile src={artist.profileImage} artistName={artist.name} />
             </li>
