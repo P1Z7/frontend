e0a507e9478824748b9a783d4f94660e0f7ba4d2
Haneul-Kim <4188989@naver.com>
2024-01-25 11:33:58 +0900

    ✨ 무한스크롤 및 scroll-snap 적용

--- app/(route)/setting/favorite/page.tsx
+++ app/(route)/setting/favorite/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 
 import classNames from "classnames";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import useInfScroll from "@/hooks/useInfScroll";
 
 const INITIAL = ["정우"];
 
 const FavoritePage = () => {
   const [favorite, setFavorite] = useState<string[]>(INITIAL);
-
   const handleClick = (name: string) => () => {
     if (favorite.includes(name)) {
       setFavorite((prev) => prev.filter((item) => item !== name));
@@ -15,17 +15,29 @@ const FavoritePage = () => {
     }
     setFavorite((prev) => [...prev, name]);
   };
+
+  const { isVisible, setIsVisible, infRef } = useInfScroll();
+  const [cursorId, setCursorId] = useState(12);
+
+  useEffect(() => {
+    if (cursorId > MOCK.length) return;
+    if (isVisible) {
+      setCursorId((prev) => prev + 6);
+    }
+  }, [isVisible]);
+
   return (
     <div className="flex flex-col gap-24 overflow-auto py-24">
       <button className="w-max text-14 font-600 text-gray-500 underline underline-offset-2">{"찾으시는 아티스트가 없나요? >"}</button>
-      <div className="grid h-5/6 grid-cols-3 gap-20 self-start overflow-auto px-16">
-        {MOCK.map((entity) => (
-          <button onClick={handleClick(entity.name)} key={entity.name} className="flex w-max flex-col items-center gap-8">
+      <div className="grid h-5/6 snap-y snap-mandatory grid-cols-3 gap-20 self-start overflow-auto px-16">
+        {MOCK.slice(0, cursorId).map((entity) => (
+          <button onClick={handleClick(entity.name)} key={entity.name} className="flex w-max snap-start flex-col items-center gap-8">
             <div className={classNames("h-80 w-80 rounded-full bg-gray-300", { "border-[1px] border-solid border-black": favorite.includes(entity.name) })} />
             {/* <Image src={entity.profileImage} alt={`${entity.name} 사진`} /> */}
             <p className="text-16 font-600">{entity.name}</p>
           </button>
         ))}
+        <div ref={infRef} />
       </div>
       <button className={classNames("rounded-sm px-16 py-12 text-16", { "bg-black text-white": favorite.length }, { "bg-gray-300 text-black": !favorite.length })}>변경하기</button>
     </div>
