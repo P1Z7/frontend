4eb7fcfb94dace7882909fba1c9d14a1626c5262
안희원 <hw7666@naver.com>
2024-01-26 23:14:48 +0900

    ♻️ refactor: post 페이지-step4 input 컴포넌트로 변경

--- app/_components/input/InputFile.tsx
+++ app/_components/input/InputFile.tsx
@@ -1,21 +1,30 @@
 import Image from "next/image";
-import { ChangeEvent, KeyboardEvent, ReactNode } from "react";
+import { ChangeEvent, Dispatch, KeyboardEvent, ReactNode, SetStateAction } from "react";
 import { FieldPath, FieldValues, UseControllerProps, useController } from "react-hook-form";
 import defaultImg from "@/public/icon/add-image_gray.svg";
 
 interface Props {
   children?: ReactNode;
+  setState: Dispatch<SetStateAction<File[]>>;
 }
 
 type Function = <TFieldValues extends FieldValues = FieldValues, TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>>(
   prop: UseControllerProps<TFieldValues, TName> & Props,
 ) => ReactNode;
 
-const InputFile: Function = ({ children, ...props }) => {
+const InputFile: Function = ({ children, setState, ...props }) => {
   const { field } = useController(props);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     field.onChange({ target: { value: e.target.files, name: field.name } });
+    if (!e.target.files) {
+      return;
+    }
+    const list: File[] = [];
+    for (let i = 0; i < e.target.files.length; i++) {
+      list.push(e.target.files[i]);
+    }
+    setState((prev) => [...prev, ...list]);
   };
 
   const handleKeyDown = (e: KeyboardEvent) => {
