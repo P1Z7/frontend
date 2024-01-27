fde9b66a06ee4ed7a406fddf4b0b176e0c6ef874
김다은 <kde98892@gmail.com>
2024-01-27 16:21:04 +0900

    ✨ feat(ReqNewArtistModal.tsx): 모달 추가

--- app/(route)/signup/_components/step/AccountInfo.tsx
+++ app/(route)/signup/_components/step/AccountInfo.tsx
@@ -6,7 +6,7 @@ import { SignUpFormType } from "@/types/index";
 export const AccountInfo = ({ onNext }: { onNext: () => void }) => {
   const { formState, control, getValues } = useFormContext<SignUpFormType>();
 
-  const isButtonDisabled = !!formState.errors.email || !!formState.errors.password || !!formState.errors.passwordCh || !!formState.isValid;
+  const isButtonDisabled = !!formState.errors.email || !!formState.errors.password || !!formState.errors.passwordCh || !formState.isDirty;
 
   return (
     <div className="flex flex-col gap-24 p-12">
