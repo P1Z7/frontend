36ab70aa94b5ad84500cf3272d392664a8cca613
김나은 <nicolekim0115@gmail.com>
2024-01-29 02:06:35 +0900

    ⚡️ fix: 첫날 안보이는 데이터 수정

--- app/(route)/mypage/_components/EventCalendar.tsx
+++ app/(route)/mypage/_components/EventCalendar.tsx
@@ -3,29 +3,44 @@
 import { useState } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
+import EventCard from "@/components/card/EventCard";
 
 interface ScheduleData {
+  placeName: string;
+  artistName: string;
+  eventType: string;
+  address: string;
   startDate: Date;
   endDate: Date;
-  eventName: string;
 }
 
 const mockData: ScheduleData[] = [
   // new Date("2024-01-28") -> 이런식으로 작성하면 startDate가 포함이 안되는 문제 발생... 왜그런지 아직 해결방법 찾지 못함 ㅠㅠ
+  // new Date(2024, 0, 28) -> 이런식으로 작성하면 해결 가능..
   {
+    placeName: "윤정한 카페",
+    artistName: "윤정한",
+    eventType: "생일카페",
+    address: "마포구",
     startDate: new Date(2024, 0, 25),
-    endDate: new Date(2024, 1, 2),
-    eventName: "정한 생일카페",
+    endDate: new Date(2024, 1, 1),
   },
+
   {
-    startDate: new Date(2024, 0, 27),
+    placeName: "김정우 카페",
+    artistName: "김정우",
+    eventType: "생일카페",
+    address: "마포구",
+    startDate: new Date(2024, 0, 26),
     endDate: new Date(2024, 0, 29),
-    eventName: "정우 생일카페",
   },
   {
-    startDate: new Date(2024, 0, 25),
-    endDate: new Date(2024, 0, 27),
-    eventName: "민지 생일카페",
+    placeName: "김민지 카페",
+    artistName: "김민지",
+    eventType: "생일카페",
+    address: "마포구",
+    startDate: new Date(2024, 0, 29),
+    endDate: new Date(2024, 0, 31),
   },
 ];
 
@@ -38,24 +53,14 @@ const EventCalendar = () => {
   };
 
   const tileContent = ({ date }: { date: Date }) => {
-    // 날짜를 선택하지 않았을 때 모든 Event를 보여줌
-    if (date === null) {
-      return (
-        <div>
-          {mockData.map((event, index) => (
-            <div key={index}>{event.eventName}</div>
-          ))}
-        </div>
-      );
-    }
+    const eventsForDate = mockData.filter((event) => new Date(event.startDate).getTime() <= date.getTime() && new Date(event.endDate).getTime() >= date.getTime());
 
-    const eventsForDate = mockData.filter((event) => event.startDate.getTime() <= date.getTime() && event.endDate.getTime() >= date.getTime());
-
+    // 추후 장소 이름 말고 디자인으로 수정 예정
     if (eventsForDate.length > 0) {
       return (
         <div>
           {eventsForDate.map((event, index) => (
-            <div key={index}>{event.eventName}</div>
+            <div key={index}>{event.placeName}</div>
           ))}
         </div>
       );
@@ -69,10 +74,19 @@ const EventCalendar = () => {
       <Calendar onChange={handleClickToday} value={date} tileContent={tileContent} />
       <div>
         <ul>
+          {/* 날짜를 선택하지 않았을 때 모든 Event를 보여줌 */}
           {mockData
-            .filter((event) => !date || (event.startDate.getTime() <= date.getTime() && event.endDate.getTime() >= date.getTime()))
+            .filter((event) => !date || (new Date(event.startDate).getTime() <= date.getTime() && new Date(event.endDate).getTime() >= date.getTime()))
             .map((event, index) => (
-              <li key={index}>{event.eventName}</li>
+              <EventCard
+                key={index}
+                placeName={event.placeName}
+                artistName={event.artistName}
+                eventType={event.eventType}
+                address={event.address}
+                startDate={event.startDate}
+                endDate={event.endDate}
+              />
             ))}
         </ul>
       </div>
