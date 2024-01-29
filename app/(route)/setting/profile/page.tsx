ae381945160d358a964eb6591a73bd39b7f11e6a
Haneul-Kim <4188989@naver.com>
2024-01-29 14:43:39 +0900

    ✨ feat: setting/profile 페이지 React-hook-form 적용

--- app/(route)/setting/profile/page.tsx
+++ app/(route)/setting/profile/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import Image from "next/image";
+import { useEffect, useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import InputFile from "@/components/input/InputFile";
 import InputText from "@/components/input/InputText";
@@ -16,30 +18,53 @@ const PROFILE_DEFAULT = {
 type DefaultValues = (typeof PROFILE_DEFAULT)["defaultValues"];
 
 const ProfilePage = () => {
-  const { formState, control, handleSubmit } = useForm(PROFILE_DEFAULT);
-  console.log(formState.isValid);
-
-  const isError = !!formState.errors.profileImage || !!formState.errors.nickname || !formState.isValid;
+  const { formState, control, handleSubmit, watch } = useForm(PROFILE_DEFAULT);
 
   const handleProfileSubmit: SubmitHandler<DefaultValues> = async ({ profileImage, nickname }) => {
     console.log(profileImage, nickname);
   };
 
+  const [newFile] = watch("profileImage") as any;
+
+  const [thumbnail, setThumbnail] = useState(newFile);
+
+  useEffect(() => {
+    if (newFile) {
+      const newURL = URL.createObjectURL(newFile);
+      setThumbnail(newURL);
+    }
+
+    return () => {
+      URL.revokeObjectURL(thumbnail);
+    };
+  }, [newFile]);
+
+  const nicknameRules = formState.dirtyFields.profileImage
+    ? { required: false }
+    : {
+        required: ERROR_MESSAGES.nickname.nicknameField,
+        pattern: { value: REG_EXP.CHECK_NICKNAME, message: ERROR_MESSAGES.nickname.nicknamePattern },
+        maxLength: { value: 10, message: ERROR_MESSAGES.nickname.nicknamePattern },
+      };
+
   return (
     <form onSubmit={handleSubmit(handleProfileSubmit)} className="flex flex-col gap-24 py-60">
-      <InputFile control={control} name="profileImage" rules={{ required: "입력" }}>
-        프로필 사진
-      </InputFile>
-      <InputText
-        name="nickname"
-        control={control}
-        maxLength={10}
-        rules={{
-          required: ERROR_MESSAGES.nickname.nicknameField,
-          pattern: { value: REG_EXP.CHECK_NICKNAME, message: ERROR_MESSAGES.nickname.nicknamePattern },
-          maxLength: { value: 10, message: ERROR_MESSAGES.nickname.nicknamePattern },
-        }}
-      >
+      <div className="relative flex flex-col">
+        <InputFile control={control} name="profileImage">
+          프로필 사진
+        </InputFile>
+        <Image
+          src={thumbnail || "/icon/no-profile.svg"}
+          width={100}
+          height={100}
+          alt="설정할 프로필 이미지"
+          className="pointer-events-none absolute left-1/2 top-1/2 -mt-[0.2rem] h-100 -translate-x-1/2 -translate-y-1/2 rounded-full object-cover"
+        />
+        <button type="button" onClick={() => setThumbnail("")} className="w-max self-center text-14 underline">
+          기본 이미지로 설정하기
+        </button>
+      </div>
+      <InputText name="nickname" control={control} maxLength={10} rules={nicknameRules}>
         닉네임
       </InputText>
       <button className={`rounded-sm bg-black px-16 py-12 text-16 text-white`}>변경하기</button>
