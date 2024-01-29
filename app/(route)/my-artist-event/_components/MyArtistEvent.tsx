4248b391850b14098a243dcd4d2fb49dc44d20d3
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2024-01-30 00:59:34 +0900

    ✨ feat: 가로형 카드 생성

--- app/(route)/my-artist-event/_components/MyArtistEvent.tsx
+++ app/(route)/my-artist-event/_components/MyArtistEvent.tsx
@@ -1,5 +1,5 @@
-import { EventMockData } from "@/(route)/_components/EventMockData";
-import VerticalEventCard from "@/components/card/VerticalEventCard";
+import { EventMockData } from "@/components/card/EventMockData";
+import HorizontalEventCard from "@/components/card/HorizontalEventCard";
 
 const MyArtistEvent = () => {
   // 좋아요 한 아티스트의 새 행사 로직은 추후 구현
@@ -8,13 +8,14 @@ const MyArtistEvent = () => {
     <>
       {EventMockData.map((event, index) => (
         <div key={index}>
-          <VerticalEventCard
+          <HorizontalEventCard
             placeName={event.placeName}
             artistName={event.artistName}
             eventType={event.eventType}
             address={event.address}
             startDate={event.startDate}
             endDate={event.endDate}
+            gifts={event.gifts}
           />
         </div>
       ))}
