542d0198c5d440e623e0f71024c0254449bf207f
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2024-02-02 02:36:32 +0900

    💄 design: 세로형 카드 디자인 수정

--- app/_components/card/VerticalEventCard.tsx
+++ app/_components/card/VerticalEventCard.tsx
@@ -1,4 +1,6 @@
+import Image from "next/image";
 import { EventCardType } from "@/types/index";
+import HeartButton from "../button/HeartButton";
 import Chip from "./Chip";
 
 interface Props {
@@ -13,18 +15,32 @@ const VerticalEventCard = ({ data }: Props) => {
   const formattedEndDate = `${endDate[1]}.${endDate[2]}`;
 
   return (
-    <div className="flex w-180 flex-col gap-8 border border-solid border-black p-[10px]">
-      <div className="h-160 bg-[#e7e7e7]" />
+    <div className="border-black flex w-148 flex-col gap-12">
+      <div className="relative h-196 w-148">
+        <div className="absolute right-8 top-8 z-nav">
+          <HeartButton />
+        </div>
+        <Image
+          src={data.eventImage}
+          fill
+          style={{
+            objectFit: "cover",
+          }}
+          alt="행사 포스터"
+          className="rounded-sm bg-gray-400"
+        />
+      </div>
       <div className="flex flex-col gap-4">
-        <p>{data.placeName}</p>
-        <div className="flex">
-          <p className="border-r border-solid border-black pr-4">
+        <p className="truncate text-16 font-600 text-gray-900">{data.placeName}</p>
+        <div className="flex gap-8 text-12 font-600 text-gray-400">
+          <p className="border-r border-gray-400 pr-8">
             {formattedStartDate} ~ {formattedEndDate}
           </p>
-          <p className="pl-4">{data.address}</p>
+          <p>{data.address}</p>
         </div>
-        <div className="flex gap-4">
-          <p>{data.artistName}</p>
+        <div className="flex gap-8">
+          <p className="text-16 font-600 text-gray-900">{data.artistName}</p>
+          {/* 공통 컴포넌트로 수정 예정 */}
           <Chip chipName={data.eventType} />
         </div>
       </div>
