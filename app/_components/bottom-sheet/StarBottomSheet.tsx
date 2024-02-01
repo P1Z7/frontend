963115cdab179d862121391f3f61870b804562de
안희원 <hw7666@naver.com>
2024-02-01 18:32:47 +0900

    💄 design: searchInput 완성

--- app/_components/bottom-sheet/StarBottomSheet.tsx
+++ app/_components/bottom-sheet/StarBottomSheet.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import SearchInput from "../input/SearchInput";
 import BottomSheetFrame from "./BottomSheetFrame";
 
@@ -6,11 +7,14 @@ interface Props {
 }
 const StarBottomSheet = ({ closeBottomSheet }: Props) => {
   //group데이터 API 콜
+  const [keyword, setKeyword] = useState("");
+
+  console.log(keyword);
 
   return (
     <BottomSheetFrame closeBottomSheet={closeBottomSheet}>
       <div className="flex flex-col gap-20 p-20 text-16" onClick={(event) => event.stopPropagation()}>
-        <SearchInput />
+        <SearchInput setKeyword={setKeyword} />
         <div>안녕 연예인 목록이야!!</div>
         <button className="flex h-48 items-center justify-center rounded-sm border-2 p-16">선택완료</button>
       </div>
--- app/_components/input/SearchInput.tsx
+++ app/_components/input/SearchInput.tsx
@@ -1,5 +1,48 @@
-const SearchInput = () => {
-  return <input className="h-48 w-full rounded-full bg-gray-200 px-16 py-12" placeholder="연예인을 검색해보세요." />;
+import Image from "next/image";
+import { Dispatch, SetStateAction } from "react";
+import { KeyboardEvent } from "react";
+import { useForm } from "react-hook-form";
+import CloseIcon from "@/public/icon/close.svg";
+import SearchIcon from "@/public/icon/search.svg";
+
+interface Props {
+  setKeyword: Dispatch<SetStateAction<string>>;
+  placeholder?: string;
+}
+
+const SearchInput = ({ setKeyword, placeholder = "검색어를 입력하세요." }: Props) => {
+  const { register, getValues, setValue, watch } = useForm();
+  const { search } = watch();
+
+  const handleSearchEnter = (event: KeyboardEvent) => {
+    if (event.keyCode === 13) {
+      setKeyword(getValues("search"));
+    }
+  };
+
+  const handleCloseClick = () => {
+    setKeyword("");
+    setValue("search", "");
+  };
+
+  return (
+    <div className="relative">
+      <input
+        className="text-black-white placeholder:gray-200 h-48 w-full rounded-full bg-gray-50 px-16 py-12 pr-68 shadow-md focus:outline-none"
+        placeholder={placeholder}
+        {...register("search")}
+        onKeyDown={handleSearchEnter}
+      />
+      <button className="absolute right-16 top-12" onClick={() => setKeyword(getValues("search"))}>
+        <SearchIcon />
+      </button>
+      {search && (
+        <button className="absolute right-48 top-12" onClick={handleCloseClick}>
+          <CloseIcon stroke="#A0A5B1" />
+        </button>
+      )}
+    </div>
+  );
 };
 
 export default SearchInput;
