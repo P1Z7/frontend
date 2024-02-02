8ab67655ec6cff6d93d90ec83f71bbcc50fe2a6e
안희원 <hw7666@naver.com>
2024-02-03 03:17:44 +0900

    💄 design: post 페이지 title UI 수정

--- app/(route)/(header)/post/page.tsx
+++ app/(route)/(header)/post/page.tsx
@@ -1,9 +1,8 @@
 "use client";
 
-// import { POST_STEPS } from "app/_constants/post";
 import Image from "next/image";
 import { useFunnel } from "@/hooks/useFunnel";
-import { SignupStepNameType } from "@/types/index";
+import { PostStepNameType } from "@/types/index";
 import GenericForm from "../signup/_components/GenericForm";
 import DetailInfo from "./_components/DetailInfo";
 import MainInfo from "./_components/MainInfo";
@@ -27,12 +26,12 @@ const DEFAULT_INPUT_VALUES = {
   detailText: "",
 };
 
-const POST_STEPS: SignupStepNameType[] = ["계정 정보", "프로필 정보", "아티스트 선택"];
+const POST_STEPS: PostStepNameType[] = ["행사 대상", "행사 정보", "특전 정보", "상세 설명"];
 
 export type PostType = Omit<typeof DEFAULT_INPUT_VALUES, "gift" | "images" | "member"> & { gift: string[]; images: (File | string)[]; member: string[] };
 
 const Post = () => {
-  const { Funnel, Step, setStep, currentStep } = useFunnel(POST_STEPS);
+  const { Funnel, Step, setStep, currentStep } = useFunnel<PostStepNameType>(POST_STEPS);
 
   const handlePrevClick = () => {
     currentStep === POST_STEPS[0] ? window.history.back() : setStep(POST_STEPS[POST_STEPS.indexOf(currentStep) - 1]);
@@ -42,7 +41,7 @@ const Post = () => {
     <div className="flex h-svh flex-col gap-24 p-20 text-16">
       <div className="flex gap-8 pb-20 pt-12">
         <Image src="/icon/left-arrow.svg" alt="뒤로가기 버튼" width={24} height={24} onClick={handlePrevClick} className="cursor-pointer" />
-        <div className="font-900 text-20">등록하기</div>
+        <div className="text-20 font-900">등록하기</div>
       </div>
       <GenericForm formOptions={{ mode: "onBlur", defaultValues: DEFAULT_INPUT_VALUES, shouldFocusError: true }}>
         <Funnel>
