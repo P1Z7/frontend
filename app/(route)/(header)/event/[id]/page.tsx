0ff9ea69da807e709e7af8c40c1e925bf726e9df
임건우 <geonulim@gmail.com>
2024-02-03 02:20:17 +0900

    💄 design: Banner 디자인 완성

--- app/(route)/(header)/event/[id]/page.tsx
+++ app/(route)/(header)/event/[id]/page.tsx
@@ -1,25 +1,14 @@
 import Tabs from "@/components/Tabs";
+import { MOCK_EVENTS } from "@/constants/mock";
 import Banner from "./_components/Banner";
 import DescriptionTab from "./_components/tab/DescriptionTab";
 import LocationTab from "./_components/tab/LocationTab";
 import ReviewTab from "./_components/tab/ReviewTab";
 
-const EVENT_MOCK = {
-  placeName: "스타벅스",
-  eventType: "카페",
-  artistName: "민지",
-  startDate: "2024-01-29",
-  endDate: "2024-01-30",
-  address: "서울특별시 마포구 와우산로 00-00 1층",
-  link: "https://winter2024.com",
-  gifts: ["포토카드", "엽서", "컵홀더"],
-  eventImage: "https://thumb.mtstarnews.com/06/2023/09/2023090715013844673_1.jpg/dims/optimize",
-};
-
 const EventInfoPage = () => {
   return (
     <>
-      <Banner data={EVENT_MOCK} />
+      <Banner data={MOCK_EVENTS[0]} />
       <Tabs names={["행사정보", "위치", "후기"]}>
         <DescriptionTab />
         <LocationTab name="을지로 위워크" address="서울 중구 삼일대로 343" />
--- app/_utils/formatString.ts
+++ app/_utils/formatString.ts
@@ -1,6 +1,10 @@
-export const formatDate = (startDate: string, endDate: string) => {
+export const formatDate = (startDate: string, endDate: string, extend: boolean = false) => {
   const start = startDate.split("-");
   const end = endDate.split("-");
+
+  if (extend) {
+    return `${start[0].slice(2)}.${start[1]}.${start[2]} ~ ${end[0].slice(2)}.${end[1]}.${end[2]}`;
+  }
 
   return `${start[1]}.${start[2]} ~ ${end[1]}.${end[2]}`;
 };
