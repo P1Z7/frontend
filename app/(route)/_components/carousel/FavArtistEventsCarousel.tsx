cc582b76ec065d84b80496cf7c79ef4cb36083a2
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2024-02-03 23:32:01 +0900

    💄 design: hero 이미지파트 디자인

--- app/(route)/_components/carousel/FavArtistEventsCarousel.tsx
+++ app/(route)/_components/carousel/FavArtistEventsCarousel.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 import Carousel from "@/components/Carousel";
 import VerticalEventCard from "@/components/card/VerticalEventCard";
 import { MOCK_EVENTS } from "@/constants/mock";
+import Hero from "@/public/icon/hero.svg";
 
 const FavArtistEventsCarousel = () => {
   // 추후 next auth로 변경 예정
@@ -13,7 +15,6 @@ const FavArtistEventsCarousel = () => {
   // 목업의 길이를 기반으로 좋아요한 이벤트의 수 확인
   // 추후 수정 예정
   const hasFavoriteEvents = MOCK_EVENTS.length > 0;
-  // const hasFavoriteEvents = false;
 
   const renderContent = () => {
     if (!status) {
@@ -21,7 +22,7 @@ const FavArtistEventsCarousel = () => {
     }
 
     if (!hasFavoriteEvents) {
-      <NoFavCard buttonName="아티스트 둘러보기" href={"/setting/artist"} />;
+      return <NoFavCard buttonName="아티스트 둘러보기" href={"/setting/artist"} />;
     }
 
     return (
@@ -39,9 +40,11 @@ const FavArtistEventsCarousel = () => {
     <div className="flex flex-col gap-16">
       <div className="flex items-center justify-between self-stretch">
         <h2 className="text-20 font-700 text-gray-900">좋아요한 아티스트의 새 행사</h2>
-        <Link href="/my-artist-event" className="text-12 font-600 text-blue">
-          전체보기
-        </Link>
+        {hasFavoriteEvents && (
+          <Link href="/my-artist-event" className="text-12 font-600 text-blue">
+            전체보기
+          </Link>
+        )}
       </div>
       {renderContent()}
     </div>
@@ -54,11 +57,21 @@ interface NoFavCardProps {
 }
 
 const NoFavCard = ({ href, buttonName }: NoFavCardProps) => {
+  const router = useRouter();
+
   return (
-    <div className="flex h-148 w-320 items-center justify-center bg-[#EFEFEF]">
-      <Link href={href} className="text-white block h-32 w-120 rounded-[4px] bg-[#676767] text-center text-14 leading-[32px]">
-        {buttonName}
-      </Link>
+    <div className="relative w-320">
+      <Hero className="shadow-hero" />
+      <div className="flex-center absolute top-32 w-full flex-col gap-16">
+        <p className="text-center text-16 font-500 text-white-black">
+          좋아하는 아티스트의 행사를
+          <br />
+          가장 먼저 축하해보세요!
+        </p>
+        <div onClick={() => router.push(href)} className="h-32 cursor-pointer rounded-full bg-gray-900 px-16 text-14 font-600 leading-loose text-white-white ">
+          {buttonName}
+        </div>
+      </div>
     </div>
   );
 };
