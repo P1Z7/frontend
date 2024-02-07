af095cd872fda6aaa704003b66d7453340c67ba2
안희원 <hw7666@naver.com>
2024-02-08 00:52:35 +0900

    💄 design: 수정요청 페이지 UI 구현 (아티스트 제외)

---
+++ app/(route)/(header)/event/[id]/approve/_components/EditCard.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import Chip from "@/components/chip/Chip";
+import { GiftType, LabelType } from "@/types/index";
+import LinkIcon from "@/public/icon/arrow-right_lg.svg";
+
+interface Props<T> {
+  id: number | string; //아마도 실제 db에선 string형식일듯
+  type: T;
+  editContent: ContentType<T>;
+  count: {
+    approve: number;
+    decline: number;
+  };
+}
+
+const EditCard = <T extends LabelType>({ id, type, editContent, count }: Props<T>) => {
+  const curPath = usePathname();
+
+  return (
+    <div className="relative flex w-full flex-col gap-4 border-b border-gray-100 py-12 pr-24 text-14 font-500">
+      <div className="text-gray-500">{type}</div>
+      {type === "특전" && (
+        <div className="flex flex-wrap gap-4">
+          {(editContent as GiftType[]).map((gift: GiftType) => (
+            <Chip key={gift} kind="goods" label={gift} />
+          ))}
+        </div>
+      )}
+      {type === "이미지" && (
+        <div className="flex gap-8 overflow-x-scroll scrollbar-hide">
+          {(editContent as string[]).map((url: string) => (
+            <div key={url} className="relative h-120 w-120 flex-shrink-0">
+              <Image alt="수정된 행사 이미지" src={url} fill className="object-cover" />
+            </div>
+          ))}
+        </div>
+      )}
+      {typeof editContent === "string" && <div className="truncate text-16 text-gray-900">{editContent}</div>}
+      <div className="flex gap-12">
+        <div className="text-blue">승인: {count.approve}</div>
+        <div className="text-red">거절: {count.decline}</div>
+      </div>
+      <button className="absolute right-0 top-0 flex h-full items-center">
+        <Link href={`${curPath}/${id}`}>
+          <LinkIcon />
+        </Link>
+      </button>
+    </div>
+  );
+};
+
+export default EditCard;
+
+type ContentType<T> = T extends "특전" ? GiftType[] : T extends "이미지" ? string[] : string;
---
+++ app/(route)/(header)/event/[id]/approve/page.tsx
@@ -0,0 +1,59 @@
+import { LabelType } from "@/types/index";
+import EditCard from "./_components/EditCard";
+
+const MOCK = [
+  {
+    id: 1,
+    type: "수정 위치",
+    editContent: "수정내용",
+    count: {
+      approve: 2,
+      decline: 2,
+    },
+  },
+  {
+    id: 2,
+    type: "장소",
+    editContent: "서울특별시 마포구 연남동 00길 00로",
+    count: {
+      approve: 2,
+      decline: 2,
+    },
+  },
+  {
+    id: 3,
+    type: "특전",
+    editContent: ["컵홀더", "포스터", "포토카드", "굿즈", "엽서", "스티커"],
+    count: {
+      approve: 2,
+      decline: 2,
+    },
+  },
+  {
+    id: 4,
+    type: "이미지",
+    editContent: [
+      "https://mblogthumb-phinf.pstatic.net/MjAyMzAxMTVfMjUx/MDAxNjczNzY2MDg5NTUz.uYXNN7SABbsOL8yH7RhV6Bt1VlDGNfhzNs5RKvG20_Yg.tb6C2XC6XaDCvHZEwxkdMpmhtdFQEUzLUgvx-FgGmY0g.JPEG.geniuslab9339/IMG_0154.JPG?type=w800",
+      "https://mblogthumb-phinf.pstatic.net/MjAyMzAxMTVfMTE2/MDAxNjczNzY2MDkxMDYy.7LNfUsjgwk7E0Qbn0QZUx2HwYY3AC1O6-sn0VqQlXKAg.Mb6uCsVc19Hv7wrXrCRyDl-FQfIT6euQZ3TUqucqFksg.JPEG.geniuslab9339/IMG_0157.JPG?type=w800",
+      "https://i.namu.wiki/i/d8HsEuu-iKCf-yz1LXvbp9hsa9a5edPe2q1IyE5bPGXB_td5IaNGY6tjJ4DncCisWYS4wiM7_VjQ8W8XQw_kcU6K3Gn0i88AbUNrxmPuUGxPA-engXgEhDFMqY0dymbxTpNBeudfIKzpbf5VWZTtEw.webp",
+      "https://i.namu.wiki/i/LUXxa23rxkYjZlLk2gQ8vZmoQ7le6KGUJ4qaOd6NraSs5x4CpcwL-mozX-xvvKW7V64KFkZgMjCAb4lCOYoEEsS7-Dj71wpUie2sdaFOC9nGO-XmPJEjgqJr8jdjCFBj9Zvw8c63hqkSU8Nj_tasSw.webp",
+      "https://i.namu.wiki/i/9BZconeaF4xyV6saVQ1H6-JwvgbVPAvMfgblsfYv8MX8xkyQFoIeGEvFo3ZLtQN6QAvJYCFSTtZiks5EZAIbPc-vRPpDwhzYpxVHs01XEhCKybm__aSMHOEywnWuiOueTgOi3eCiaEacTh1a-vtFtw.webp",
+    ],
+    count: {
+      approve: 2,
+      decline: 2,
+    },
+  },
+];
+
+const EditApprove = () => {
+  return (
+    <div className="px-20 py-16">
+      {MOCK.map(({ id, type, editContent, count }) => (
+        <EditCard key={id} id={id} type={type as LabelType} editContent={editContent} count={count} />
+      ))}
+    </div>
+  );
+};
+
+export default EditApprove;
