35b4396443d557ec171fa0dfdec53c716a3bea01
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2024-02-08 12:53:11 +0900

    ✨ feat(ArtistList): 아티스트 리스트 무한스크롤 빼고 api 연결

--- app/(route)/_components/artist-list/ArtistList.tsx
+++ app/(route)/_components/artist-list/ArtistList.tsx
@@ -1,34 +1,29 @@
 "use client";
 
-import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 import InfiniteScroll from "react-infinite-scroller";
 import ArtistCard from "@/components/ArtistCard";
-import { ProfileMockData } from "./ProfileMockData";
+import { Api } from "@/api/api";
+import { ArtistAndGroupListType } from "@/types/index";
 
 const ArtistList = () => {
-  const [artists, setArtists] = useState(ProfileMockData);
-  const [hasMore, setHasMore] = useState(true);
+  const instance = new Api(process.env.NEXT_PUBLIC_ACCESS_TOKEN);
 
-  // 이부분 바꿔야함 일단 무한스크롤 잘 되는지 테스트용..
-  const loadMore = () => {
-    const newArtists = [...artists, ...ProfileMockData];
-
-    // 데이터가 9개 이상이면 무한스크롤 계속 진행
-    if (newArtists.length >= 9) {
-      setHasMore(true);
-    } else {
-      // 9개 미만이면 더 이상 무한스크롤 하지 않음
-      setHasMore(false);
-    }
-
-    // 새로운 아티스트 데이터를 적용
-    setArtists(newArtists);
-  };
+  const {
+    data: artistData,
+    isSuccess,
+    isLoading,
+  } = useQuery<ArtistAndGroupListType>({
+    queryKey: ["artist"],
+    queryFn: async () => {
+      return instance.get("/artist/group", { size: 12, page: 1 });
+    },
+  });
 
   return (
     <div className="flex flex-col gap-16 px-20">
       <h2 className="text-20 font-700 text-gray-900">아티스트로 찾아보기</h2>
-      <InfiniteScroll
+      {/* <InfiniteScroll
         pageStart={0}
         loadMore={loadMore}
         hasMore={hasMore}
@@ -38,17 +33,22 @@ const ArtistList = () => {
           </div>
         }
         className="flex flex-col items-center"
-      >
+      > */}
+      <div className="flex flex-col items-center">
         <div className="flex w-full max-w-[52rem] flex-col items-center">
-          <ul className="flex flex-wrap justify-center gap-20">
-            {artists.map((artist, index) => (
-              <li key={index}>
-                <ArtistCard profileImage={artist.profileImage}>{artist.name}</ArtistCard>
-              </li>
-            ))}
-          </ul>
+          {isLoading && <div>로딩중</div>}
+          {isSuccess && (
+            <ul className="flex flex-wrap justify-center gap-20">
+              {artistData?.artistAndGroupList.map((artist) => (
+                <li key={artist.id} className="w-88">
+                  <ArtistCard profileImage={artist.image}>{artist.name}</ArtistCard>
+                </li>
+              ))}
+            </ul>
+          )}
         </div>
-      </InfiniteScroll>
+      </div>
+      {/* </InfiniteScroll> */}
     </div>
   );
 };
