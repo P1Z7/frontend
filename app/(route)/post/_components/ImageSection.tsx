52cf22a56a1c48f22d8e5b7dfb65316b2446b4a1
안희원 <hw7666@naver.com>
2024-02-08 18:26:41 +0900

    💄 design: 이미지 부분 삭제 아이콘 & 스크롤 범위 변경

--- app/(route)/post/_components/ImageSection.tsx
+++ app/(route)/post/_components/ImageSection.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Dispatch, SetStateAction } from "react";
 import InputFile from "@/components/input/InputFile";
+import LeftIcon from "@/public/icon/arrow-left_sm.svg";
 import CloseIcon from "@/public/icon/close.svg";
 
 interface Props {
@@ -12,16 +13,20 @@ const ImageSection = ({ imgList, setImgList }: Props) => {
   return (
     <div className="flex flex-col gap-8">
       <div>이미지</div>
-      <div className="flex gap-8">
+      <div className="flex gap-8 overflow-x-scroll scrollbar-hide">
         {imgList.length < 5 && <InputFile name="eventImages" />}
-        <div className="flex w-[400px] gap-8 overflow-x-scroll scrollbar-hide">
+        <div className="flex gap-8">
           {Array.from(imgList).map((file, idx) => (
-            <div key={idx} className="relative flex h-120 w-120 shrink-0" onClick={() => setImgList((prev) => prev.filter((item) => item !== file))}>
-              <button className="absolute right-4 top-4 z-nav cursor-pointer">
-                <CloseIcon width="24" height="24" stroke="#FFFFFF" />
+            <div key={idx} className="relative flex h-120 w-120 shrink-0">
+              <button
+                onClick={() => setImgList((prev) => prev.filter((item) => item !== file))}
+                type="button"
+                className="absolute right-4 top-4 z-nav flex h-24 w-24 cursor-pointer items-center justify-center rounded-full bg-gray-900 bg-opacity-65"
+              >
+                <CloseIcon width="20" height="20" stroke="#FFFFFF" />
               </button>
               {idx === 0 && <span className="absolute left-4 top-4 z-nav rounded-[0.8rem] bg-[#000000]/[.72] px-8 py-4 text-12 text-white-white">대표이미지</span>}
-              <Image src={typeof file === "string" ? file : URL.createObjectURL(file)} alt="선택한 사진 미리보기" fill className="object-cover" />
+              <Image src={typeof file === "string" ? file : URL.createObjectURL(file)} alt="선택한 이미지 미리보기" fill className="object-cover" />
             </div>
           ))}
         </div>
