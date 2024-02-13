db317d95857fa5fb81bac26979124d25a19e62e7
임건우 <geonulim@gmail.com>
2024-02-14 02:35:54 +0900

    ⚡️ fix: memo import 수정

--- app/(route)/(header)/event/[eventId]/post/_components/MemoizedImageList.tsx
+++ app/(route)/(header)/event/[eventId]/post/_components/MemoizedImageList.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import React from "react";
+import { memo } from "react";
 import CloseIcon from "@/public/icon/close.svg";
 
 interface Props {
@@ -26,4 +26,4 @@ const ImageList = ({ imageList, handleRemoveImage }: Props) => {
   );
 };
 
-export const MemoizedImageList = React.memo(ImageList);
+export const MemoizedImageList = memo(ImageList);
