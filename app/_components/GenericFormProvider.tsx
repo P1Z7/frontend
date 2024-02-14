c39e153796cd4b87c925e92817813e0a067f014a
김다은 <kde98892@gmail.com>
2024-02-14 16:29:42 +0900

    🩹 chore: 함수 분리

--- app/_components/GenericFormProvider.tsx
+++ app/_components/GenericFormProvider.tsx
@@ -3,6 +3,7 @@ import { usePathname, useRouter } from "next/navigation";
 import React from "react";
 import { FieldValues, FormProvider, UseFormProps, useForm } from "react-hook-form";
 import toast from "react-hot-toast";
+import { handleSignupSubmit } from "@/utils/handleSignupSubmit";
 import { handlePostSubmit } from "@/utils/submitPost";
 
 interface GenericFormProps<T extends FieldValues> {
@@ -16,55 +17,16 @@ const GenericFormProvider = <T extends FieldValues>({ children, formOptions }: G
   const path = usePathname();
   const instance = new Api(process.env.NEXT_PUBLIC_ACCESS_TOKEN);
 
-  const handleSignupSubmit = async () => {
-    const userInput = methods.getValues();
-    const { email, password, passwordCheck, nickName, myArtists } = userInput;
-    try {
-      const signupRes = await instance.post("/users", {
-        userName: "",
-        signupMethod: "opener",
-        email,
-        password,
-        passwordCheck,
-        nickName,
-        myArtists,
-      });
-
-      if (signupRes.error) {
-        throw new Error(signupRes.error);
-      }
-
-      const signinRes = await instance.post("/auth", {
-        email,
-        password,
-        signinMethod: "opener",
-      });
-
-      toast(`어서오세요! ${signinRes?.nickName}님`, {
-        className: "text-16 font-600",
-      });
-      router.push("/");
-    } catch (error: any) {
-      if (error.message === "exist user") {
-        toast.error("이미 존재하는 이메일입니다.", {
-          className: "text-16 font-600",
-        });
-      }
-      if (error.message === "Bad Request") {
-        toast.error("가입 정보를 다시 한 번 확인해 주세요.", {
-          className: "text-16 font-600",
-        });
-      }
-    }
-  };
-
   const onSubmit = async () => {
     if (path === "/post") {
       const res = await handlePostSubmit(methods.getValues(), instance);
       router.push(`/event/${res.eventId}`);
     }
     if (path === "/signup") {
-      const res = await handleSignupSubmit();
+      const res = await handleSignupSubmit(methods.getValues(), instance);
+      if (!res.error) {
+        router.push("/");
+      }
     }
   };
 
