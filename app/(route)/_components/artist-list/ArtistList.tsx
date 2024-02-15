9d4fc485e71bcff68d695e0d3a40d8c41ae3a161
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2024-02-15 15:50:22 +0900

    ✨ feat(ArtistList): 아티스트로 찾아보기 무한스크롤 구현

--- app/(route)/_components/artist-list/ArtistList.tsx
+++ app/(route)/_components/artist-list/ArtistList.tsx
@@ -1,59 +1,62 @@
 "use client";
 
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
-import InfiniteScroll from "react-infinite-scroller";
 import ArtistCard from "@/components/ArtistCard";
 import { Api } from "@/api/api";
+import useInfiniteScroll from "@/hooks/useInfiniteScroll";
 import { Res_Get_Type } from "@/types/getResType";
+
+const SIZE = 12;
 
 const ArtistList = () => {
   const router = useRouter();
 
-  const instance = new Api(process.env.NEXT_PUBLIC_ACCESS_TOKEN);
+  const instance = new Api();
+
+  const getArtists = async ({ pageParam = 1 }) => {
+    const data: Res_Get_Type["artistGroup"] = await instance.get("/artist/group", {
+      size: SIZE,
+      page: pageParam,
+    });
+    return data;
+  };
 
   const {
     data: artistData,
-    isSuccess,
-    isLoading,
-  } = useQuery<Res_Get_Type["artistGroup"]>({
+    fetchNextPage,
+    isFetching,
+  } = useInfiniteQuery({
+    initialPageParam: 1,
     queryKey: ["artist"],
-    queryFn: async () => {
-      return instance.get("/artist/group", { size: 12, page: 1 });
-    },
+    queryFn: getArtists,
+    getNextPageParam: (lastPage) => (lastPage.page * SIZE < lastPage.totalCount ? lastPage.page + 1 : null),
+  });
+
+  const containerRef = useInfiniteScroll({
+    handleScroll: fetchNextPage,
+    deps: [artistData],
   });
 
   return (
     <div className="flex flex-col gap-16 px-20">
       <h2 className="text-20 font-700 text-gray-900">아티스트로 찾아보기</h2>
-      {/* <InfiniteScroll
-        pageStart={0}
-        loadMore={loadMore}
-        hasMore={hasMore}
-        loader={
-          <div className="loader" key={0}>
-            Loading ...
-          </div>
-        }
-        className="flex flex-col items-center"
-      > */}
       <div className="flex flex-col items-center">
         <div className="flex w-full max-w-[52rem] flex-col items-center">
-          {isLoading && <div>로딩중</div>}
-          {isSuccess && (
-            <ul className="flex flex-wrap justify-center gap-20">
-              {artistData?.artistAndGroupList.map((artist) => (
+          <ul className="flex flex-wrap justify-center gap-20">
+            {artistData?.pages.map((page) =>
+              page.artistAndGroupList.map((artist) => (
                 <li key={artist.id} className="w-88">
                   <ArtistCard profileImage={artist.image} onClick={() => router.push(`/search?keyword=${artist.name}`)}>
                     {artist.name}
                   </ArtistCard>
                 </li>
-              ))}
-            </ul>
-          )}
+              )),
+            )}
+            <div ref={containerRef} className="h-16 w-full" />
+          </ul>
         </div>
       </div>
-      {/* </InfiniteScroll> */}
     </div>
   );
 };
