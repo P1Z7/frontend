c44e180ef7c4bc65c6751fa1d221093daf0f1fd6
안희원 <hw7666@naver.com>
2024-02-15 17:18:19 +0900

    ✨ feat: 수정승인 페이지 api 연결

--- app/(route)/(header)/event/[eventId]/approve/[editId]/page.tsx
+++ app/(route)/(header)/event/[eventId]/approve/[editId]/page.tsx
@@ -3,57 +3,96 @@
 import { useQuery } from "@tanstack/react-query";
 import Link from "next/link";
 import { useParams } from "next/navigation";
+import { useEffect } from "react";
 import { Api } from "@/api/api";
+import { CategoryType, LabelType } from "@/types/index";
+import { LABEL_BY_CATEGORY } from "@/constants/post";
 import LinkIcon from "@/public/icon/link.svg";
 import IdIcon from "@/public/icon/user.svg";
 import BottomDoubleButton from "../../_components/BottomDoubleButton";
+import { exceptionList } from "../_components/EditCard";
+import { OrganizerEdit } from "../_components/EditFormat";
 import ReactionIcon from "../_components/ReactionIcon";
+import RenderException from "../_components/RenderException";
 import EditBox from "./_components/EditBox";
 
 const EditDetailApprove = () => {
   const { editId } = useParams();
   const instance = new Api();
-  const { data } = useQuery({
+  const { data, isSuccess } = useQuery({
     queryKey: ["approveDetail", editId],
     queryFn: async () => {
       return instance.get(`/event/update/application/${editId}`, { eventUpdateApplicationId: String(editId) });
     },
   });
 
-  console.log(data);
+  useEffect(() => {
+    console.log(data);
+    // console.log(data.applicationDetail.updateCategory);
+    // console.log(data.originEvent[data.applicationDetail.updateCategory]);
+  }, [data]);
 
   return (
     <div className="flex flex-col gap-20 px-20 py-16 pb-96 text-16 font-500 text-gray-900">
-      <section className="flex flex-col gap-4">
-        장소 이름
-        <EditBox>서울특별시 마포구 연남동 00길 00로</EditBox>
-        <EditBox isEdited>서울특별시 마포구 동그라미동 네모길 세모로</EditBox>
-      </section>
-      <section className="flex flex-col gap-4">
-        승인 현황
-        <div className="flex gap-24 py-8">
-          <div className="flex gap-8 text-14 font-500 text-gray-500">
-            승인
-            <ReactionIcon count={2} type="approve" size="l" />
-          </div>
-          <div className="flex gap-8 text-14 font-500 text-gray-500">
-            거절
-            <ReactionIcon count={1} type="reject" size="l" />
-          </div>
-        </div>
-      </section>
-      <section className="flex flex-col gap-8 rounded-sm bg-gray-50 px-12 py-8 text-14">
-        행사 링크를 통해 위 편집내용이 맞는지 확인해주세요!
-        <div className="flex gap-12 text-blue">
-          <LinkIcon stroke="#A0A5B1" width="20" height="20" />
-          <Link href="https://winter2024.com">https://winter2024.com</Link>
-        </div>
-        <div className="flex gap-12">
-          <IdIcon stroke="#A0A5B1" width="20" height="20" />
-          <div>sns종류아이콘..</div>
-          @winter_2024
-        </div>
-      </section>
+      {isSuccess && (
+        <>
+          <section className="flex flex-col gap-4">
+            {LABEL_BY_CATEGORY[data.applicationDetail.updateCategory as CategoryType]}
+            <EditBox>
+              {exceptionList.includes(LABEL_BY_CATEGORY[data.applicationDetail.updateCategory as CategoryType] as LabelType) ? (
+                <RenderException
+                  editContent={JSON.parse(data.applicationDetail.updateData)}
+                  instance={instance}
+                  type={LABEL_BY_CATEGORY[data.applicationDetail.updateCategory as CategoryType] as LabelType}
+                />
+              ) : (
+                <>{data.originEvent[data.applicationDetail.updateCategory] || "기존 내용 없음"}</>
+              )}
+            </EditBox>
+            <EditBox isEdited>
+              {exceptionList.includes(LABEL_BY_CATEGORY[data.applicationDetail.updateCategory as CategoryType] as LabelType) ? (
+                <RenderException
+                  editContent={JSON.parse(data.applicationDetail.updateData)}
+                  instance={instance}
+                  type={LABEL_BY_CATEGORY[data.applicationDetail.updateCategory as CategoryType] as LabelType}
+                />
+              ) : (
+                <>{JSON.parse(data.applicationDetail.updateData)[data.applicationDetail.updateCategory]}</>
+              )}
+            </EditBox>
+          </section>
+          <section className="flex flex-col gap-4">
+            승인 현황
+            <div className="flex gap-24 py-8">
+              <div className="flex gap-8 text-14 font-500 text-gray-500">
+                승인
+                <ReactionIcon count={Number(data.applicationDetail.approvalCount)} type="approve" size="l" />
+              </div>
+              <div className="flex gap-8 text-14 font-500 text-gray-500">
+                거절
+                <ReactionIcon count={Number(data.applicationDetail.rejectionCount)} type="reject" size="l" />
+              </div>
+            </div>
+          </section>
+          {(data.originEvent.eventUrl || data.originEvent.organizerSns) && (
+            <section className="flex flex-col gap-8 rounded-sm bg-gray-50 px-12 py-8 text-14">
+              행사 링크를 통해 위 편집내용이 맞는지 확인해주세요!
+              {data.originEvent.eventUrl && (
+                <div className="flex gap-12 text-blue">
+                  <LinkIcon stroke="#A0A5B1" width="20" height="20" />
+                  <Link href={data.originEvent.eventUrl}>{data.originEvent.eventUrl}</Link>
+                </div>
+              )}
+              {data.originEvent.organizerSns && (
+                <div className="flex gap-12">
+                  <IdIcon stroke="#A0A5B1" width="20" height="20" />
+                  <OrganizerEdit snsType={data.originEvent.snsType} snsId={data.originEvent.organizerSns} />
+                </div>
+              )}
+            </section>
+          )}
+        </>
+      )}
       <BottomDoubleButton onClickLeft={() => console.log("거절")} onClickRight={() => console.log("승인")} />
     </div>
   );
--- app/(route)/(header)/event/[eventId]/approve/_components/EditCard.tsx
+++ app/(route)/(header)/event/[eventId]/approve/_components/EditCard.tsx
@@ -7,10 +7,10 @@ import { Api } from "@/api/api";
 import { findGroupName, findMemberName, findTagName } from "@/utils/findName";
 import { CategoryType, EditContentType, LabelType, PostValueType } from "@/types/index";
 import LinkIcon from "@/public/icon/arrow-right_lg.svg";
-import { AddressEdit, ArtistEdit, DateEdit, ImageEdit, OrganizerEdit, TagEdit } from "./EditFormat";
 import ReactionIcon from "./ReactionIcon";
+import RenderException from "./RenderException";
 
-const exceptionList: LabelType[] = ["아티스트", "주소", "기간", "특전", "이미지", "주최자"];
+export const exceptionList: LabelType[] = ["아티스트", "주소", "기간", "특전", "이미지", "주최자"];
 
 interface Props {
   id: string;
@@ -28,23 +28,14 @@ const EditCard = ({ id, type, editContent, count, createdAt, category }: Props) 
   const curPath = usePathname();
   const instance = new Api(process.env.NEXT_PUBLIC_ACCESS_TOKEN);
 
-  const renderException = () => {
-    return (
-      <>
-        {type === "특전" && <TagEdit tagList={findTagName(editContent.tags || [])} />}
-        {type === "이미지" && <ImageEdit imgList={editContent.eventImages || []} />}
-        {type === "아티스트" && <ArtistEdit memberList={findMemberName(instance, editContent.artists || [])} groupName={findGroupName(instance, editContent.groupId || "")} />}
-        {type === "주최자" && <OrganizerEdit snsType={editContent.snsType || "기타"} snsId={editContent.organizerSns || ""} />}
-        {type === "주소" && <AddressEdit address={editContent.address || ""} detail={editContent.addressDetail || ""} />}
-        {type === "기간" && <DateEdit startDate={editContent.startDate || ""} endDate={editContent.endDate || ""} />}
-      </>
-    );
-  };
-
   return (
     <div className="relative flex w-full flex-col gap-8 border-b border-gray-100 py-16 pr-24 text-14 font-500">
       <div className="text-gray-500">{type}</div>
-      {exceptionList.includes(type) ? renderException() : <div className="truncate text-16 text-gray-900">{editContent[category as PostValueType]}</div>}
+      {exceptionList.includes(type) ? (
+        <RenderException editContent={editContent} instance={instance} type={type} />
+      ) : (
+        <div className="truncate text-16 text-gray-900">{editContent[category as PostValueType]}</div>
+      )}
       <div className="flex items-center gap-12 text-12 text-gray-400">
         <div className="flex gap-4">
           승인
