fa11fddde12913098d71d330229b23a469f4732d
안희원 <hw7666@naver.com>
2024-02-16 21:31:49 +0900

    ⚡️ fix: editCard 전체를 링크로 변경

--- app/(route)/(header)/event/[eventId]/approve/_components/EditCard.tsx
+++ app/(route)/(header)/event/[eventId]/approve/_components/EditCard.tsx
@@ -26,30 +26,30 @@ const EditCard = ({ id, type, editContent, count, createdAt, category }: Props) 
   const curPath = usePathname();
 
   return (
-    <div className="relative flex w-full flex-col gap-8 border-b border-gray-100 py-16 pr-24 text-14 font-500">
-      <div className="text-gray-500">{type}</div>
-      {exceptionList.includes(type) ? (
-        <RenderException editContent={editContent} instance={instance} type={type} />
-      ) : (
-        <div className="truncate text-16 text-gray-900">{editContent[category as PostValueType]}</div>
-      )}
-      <div className="flex items-center gap-12 text-12 text-gray-400">
-        <div className="flex gap-4">
-          승인
-          <ReactionIcon count={count.approve} type="approve" />
+    <Link href={`${curPath}/${id}`}>
+      <div className="relative flex w-full flex-col gap-8 border-b border-gray-100 py-16 pr-24 text-14 font-500">
+        <div className="text-gray-500">{type}</div>
+        {exceptionList.includes(type) ? (
+          <RenderException editContent={editContent} instance={instance} type={type} />
+        ) : (
+          <div className="truncate text-16 text-gray-900">{editContent[category as PostValueType]}</div>
+        )}
+        <div className="flex items-center gap-12 text-12 text-gray-400">
+          <div className="flex gap-4">
+            승인
+            <ReactionIcon count={count.approve} type="approve" />
+          </div>
+          <div className="flex gap-4">
+            거절
+            <ReactionIcon count={count.decline} type="reject" />
+          </div>
+          <p className="text-gray-300">{formatDate(createdAt, "yyyy.MM.dd")}</p>
         </div>
-        <div className="flex gap-4">
-          거절
-          <ReactionIcon count={count.decline} type="reject" />
-        </div>
-        <p className="text-gray-300">{formatDate(createdAt, "yyyy.MM.dd")}</p>
-      </div>
-      <button className="absolute right-0 top-0 flex h-full items-center">
-        <Link href={`${curPath}/${id}`}>
+        <button className="absolute right-0 top-0 flex h-full items-center">
           <LinkIcon />
-        </Link>
-      </button>
-    </div>
+        </button>
+      </div>
+    </Link>
   );
 };
 
