b7f57b1280f93d217b6a383b80dbd212db5175e7
Haneul-Kim <4188989@naver.com>
2024-02-16 19:08:33 +0900

    🩹 chore: test

--- app/_hooks/useAuth.ts
+++ app/_hooks/useAuth.ts
@@ -1,16 +1,15 @@
+"use client";
+
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
 import { useSession } from "@/store/session/cookies";
 
 export const useAuth = (href: string) => {
   const router = useRouter();
   const session = useSession();
 
-  useEffect(() => {
-    if (!session) {
-      router.push(href);
-    }
-  }, []);
+  if (!session) {
+    router.push(href);
+  }
 
   return session;
 };
