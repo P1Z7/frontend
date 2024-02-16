80ef62562c20ab9f2a48bb437e4b70d3e0ef68a4
Haneul-Kim <4188989@naver.com>
2024-02-16 18:27:40 +0900

    🩹 chore: 배포 수정

--- app/_constants/confetti.ts
+++ app/_constants/confetti.ts
@@ -1,4 +1,4 @@
-import confetti, { Options } from "canvas-confetti";
+import { Options } from "canvas-confetti";
 
 export const SHOT_SIGNIN: Options[] = [
   {
@@ -19,16 +19,16 @@ export const SHOT_SIGNIN: Options[] = [
   },
 ];
 
-const heart = confetti.shapeFromPath({
-  path: "M167 72c19,-38 37,-56 75,-56 42,0 76,33 76,75 0,76 -76,151 -151,227 -76,-76 -151,-151 -151,-227 0,-42 33,-75 75,-75 38,0 57,18 76,56z",
-});
+// const heart = confetti.shapeFromPath({
+//   path: "M167 72c19,-38 37,-56 75,-56 42,0 76,33 76,75 0,76 -76,151 -151,227 -76,-76 -151,-151 -151,-227 0,-42 33,-75 75,-75 38,0 57,18 76,56z",
+// });
 
-export const SHOT_WITHDRAW: Options[] = Array<Options>(10).fill({
-  particleCount: 15,
-  scalar: 3,
-  spread: 50,
-  startVelocity: 30,
-  shapes: [heart],
-  colors: ["#EB278C", "#ff008480", "#e11e831f"],
-  disableForReducedMotion: true,
-});
+// export const SHOT_WITHDRAW: Options[] = Array<Options>(10).fill({
+//   particleCount: 15,
+//   scalar: 3,
+//   spread: 50,
+//   startVelocity: 30,
+//   shapes: [heart],
+//   colors: ["#EB278C", "#ff008480", "#e11e831f"],
+//   disableForReducedMotion: true,
+// });
