fa58cf0558a3168f2fc74e72fe5f37968156eeb5
임건우 <geonulim@gmail.com>
2024-02-16 23:24:29 +0900

    🩹 chore: 디자인 수정

--- app/_components/layout/DottedLayout.tsx
+++ app/_components/layout/DottedLayout.tsx
@@ -14,7 +14,7 @@ const DottedLayout = ({ children, type }: Props) => {
   return (
     <>
       <div className="pc:hidden">{children}</div>
-      <div className="hidden w-full bg-[url('/image/dotted-background.png')] bg-contain bg-repeat-y px-20 pc:block">
+      <div className="hidden min-h-dvh w-full bg-[url('/image/dotted-background.png')] bg-contain bg-repeat-y px-20 pc:block">
         <div className={`mx-auto ${MAX_X[type]} bg-white-black`}>{children}</div>
         <div />
       </div>
