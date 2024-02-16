9a87963dab6929f432688beec7167541be9c5138
Haneul-Kim <4188989@naver.com>
2024-02-16 20:04:32 +0900

    🩹 fix: 이제 원인을 알았음

--- app/_hooks/useAuth.ts
+++ app/_hooks/useAuth.ts
@@ -1,15 +1,16 @@
-"use client";
-
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 import { useSession } from "@/store/session/cookies";
 
 export const useAuth = (href: string) => {
   const router = useRouter();
   const session = useSession();
 
-  if (!session) {
-    router.push(href);
-  }
+  useEffect(() => {
+    if (!session) {
+      router.push(href);
+    }
+  }, [session]);
 
   return session;
 };
