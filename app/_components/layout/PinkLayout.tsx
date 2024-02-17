786da8e652b331363ce80857d2c7df5ff8c89d7c
안희원 <hw7666@naver.com>
2024-02-18 01:53:34 +0900

    💄 design: 수정 요청&승인 페이지 pc 디자인 적용

--- app/_components/layout/PinkLayout.tsx
+++ app/_components/layout/PinkLayout.tsx
@@ -3,18 +3,19 @@ import { ReactNode } from "react";
 const WIDTH = {
   narrow: "w-384",
   wide: "w-[54.8rem]",
+  middle: "w-[42.4rem]",
 };
 
 interface Props {
   children: ReactNode;
-  size: "narrow" | "wide";
+  size: "narrow" | "wide" | "middle";
 }
 
 const PinkLayout = ({ children, size }: Props) => {
   return (
     <>
       <div className="pc:hidden">{children}</div>
-      <div className="hidden min-h-[calc(100dvh-7.2rem)] w-full items-center justify-center bg-main-pink-50 bg-[url('/image/dotted-background.png')] bg-[length:100%_auto] bg-top bg-repeat-y px-20 pc:flex">
+      <div className="hidden min-h-[calc(100dvh-7.2rem)] w-full items-center justify-center bg-main-pink-50 bg-[url('/image/dotted-background.png')] bg-[length:100%_auto] bg-top bg-repeat-y px-20 py-48 pc:flex">
         <div className={`relative min-h-[66rem] rotate-0 overflow-y-scroll rounded-lg bg-white-black shadow-[0px_4px_20px_0px_rgba(0,0,0,0.12)] ${WIDTH[size]}`}>{children}</div>
       </div>
     </>
