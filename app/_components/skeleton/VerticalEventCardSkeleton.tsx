87fe07293c2f4051168ddd61ba70da41f7dd69df
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2024-02-20 03:17:22 +0900

    ⚡fix: 스켈레톤 오류 수정

--- app/_components/skeleton/VerticalEventCardSkeleton.tsx
+++ app/_components/skeleton/VerticalEventCardSkeleton.tsx
@@ -1,18 +1,25 @@
 import { Skeleton } from "@mui/material";
-import useGetWindowWidth from "@/hooks/useGetWindowWidth";
 
 const VerticalEventCardSkeleton = () => {
-  const { isPc } = useGetWindowWidth();
-
   const skeletons = [];
   for (let i = 0; i < 8; i++) {
     skeletons.push(
       <div key={i} className="flex flex-col gap-12">
-        <Skeleton variant="rounded" width={isPc ? 188 : 148} height={isPc ? 244 : 196} />
-        <div className="flex flex-col gap-4 pc:gap-8">
-          <Skeleton variant="rectangular" width={isPc ? 188 : 148} height={24} />
-          <Skeleton variant="rectangular" width={isPc ? 188 : 148} height={isPc ? 20 : 18} />
-          <Skeleton variant="rectangular" width={isPc ? 188 : 148} height={24} />
+        <div className="pc:hidden">
+          <Skeleton variant="rounded" width={148} height={196} />
+          <div className="flex flex-col gap-4 pc:gap-8">
+            <Skeleton variant="rectangular" width={148} height={24} />
+            <Skeleton variant="rectangular" width={148} height={18} />
+            <Skeleton variant="rectangular" width={148} height={24} />
+          </div>
+        </div>
+        <div className="hidden pc:block">
+          <Skeleton variant="rounded" width={188} height={244} />
+          <div className="flex flex-col gap-4 pc:gap-8">
+            <Skeleton variant="rectangular" width={188} height={24} />
+            <Skeleton variant="rectangular" width={188} height={20} />
+            <Skeleton variant="rectangular" width={188} height={24} />
+          </div>
         </div>
       </div>,
     );
