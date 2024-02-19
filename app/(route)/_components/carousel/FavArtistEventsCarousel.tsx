88e234da9366faf8383ac5c547d0d6c0cc2673dc
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2024-02-20 01:38:04 +0900

    ⚡️ fix: 배너 수정

--- app/(route)/_components/carousel/FavArtistEventsCarousel.tsx
+++ app/(route)/_components/carousel/FavArtistEventsCarousel.tsx
@@ -31,6 +31,18 @@ const FavArtistEventsCarousel = () => {
     },
   });
 
+  const { data: favArtist } = useQuery({
+    queryKey: ["favArtist"],
+    queryFn: async () => {
+      if (!session) {
+        return;
+      }
+      return instance.get(`/users/${session.user.userId}/artists`, {
+        userId: session.user.userId,
+      });
+    },
+  });
+
   useEffect(() => {
     if (session?.isAuth) setIsSignin(true);
   }, []);
@@ -41,13 +53,22 @@ const FavArtistEventsCarousel = () => {
         {isSignin && (
           <>
             <h2 className="text-20 font-700 text-gray-900">내 아티스트의 새 행사</h2>
-            <Link href="/my-artist-event?sort=최신순" className="text-12 font-600 text-blue">
-              전체보기
-            </Link>
+            {!!favArtistEvent?.length && (
+              <Link href="/my-artist-event?sort=최신순" className="text-12 font-600 text-blue">
+                전체보기
+              </Link>
+            )}
           </>
         )}
       </div>
-      <RenderContent status={isSignin} hasFavoriteEvents={!!favArtistEvent} isLoading={isLoading} isSuccess={isSuccess} favArtistEvent={favArtistEvent} />
+      <RenderContent
+        status={isSignin}
+        hasFavoriteEvents={!!favArtistEvent?.length}
+        hasFavoriteArtists={!!favArtist?.length}
+        isLoading={isLoading}
+        isSuccess={isSuccess}
+        favArtistEvent={favArtistEvent}
+      />
     </div>
   );
 };
@@ -58,11 +79,12 @@ interface RenderContentProps {
   isLoading: boolean;
   isSuccess: boolean;
   favArtistEvent?: Res_Get_Type["eventList"];
+  hasFavoriteArtists: boolean;
 }
 
-const RenderContent = ({ status, hasFavoriteEvents, isLoading, isSuccess, favArtistEvent }: RenderContentProps) => {
+const RenderContent = ({ status, hasFavoriteEvents, isLoading, isSuccess, favArtistEvent, hasFavoriteArtists }: RenderContentProps) => {
   if (!status) {
-    return <NoFavCard buttonName="로그인 하기" href={"/signin"} />;
+    return <LoginHero />;
   }
 
   return (
@@ -70,7 +92,8 @@ const RenderContent = ({ status, hasFavoriteEvents, isLoading, isSuccess, favArt
       {isLoading && <div>로딩중</div>}
       {isSuccess && (
         <>
-          {!hasFavoriteEvents && <NoFavCard buttonName="아티스트 둘러보기" href={"/setting/favorite"} />}
+          {!hasFavoriteArtists && <FollowArtistHero />}
+          {hasFavoriteArtists && !hasFavoriteEvents && <NoNewCard />}
           {hasFavoriteEvents && <Carousel cards={favArtistEvent} />}
         </>
       )}
@@ -78,12 +101,7 @@ const RenderContent = ({ status, hasFavoriteEvents, isLoading, isSuccess, favArt
   );
 };
 
-interface NoFavCardProps {
-  href: string;
-  buttonName: string;
-}
-
-export const NoFavCard = ({ href, buttonName }: NoFavCardProps) => {
+const LoginHero = () => {
   const router = useRouter();
 
   return (
@@ -91,9 +109,47 @@ export const NoFavCard = ({ href, buttonName }: NoFavCardProps) => {
       <div className="flex-center relative h-160 overflow-hidden rounded-lg border border-main-pink-50 pc:h-232">
         <Image src="/image/hero.png" fill sizes="100%" className="object-cover" alt="배너이미지" />
         <div className="flex-center absolute top-96 w-full flex-col gap-16 pc:top-152">
-          <div onClick={() => router.push(href)} className="h-32 cursor-pointer rounded-full bg-gray-900 px-16 text-14 font-600 leading-loose text-white-white pc:h-40 pc:text-18">
-            {buttonName}
-          </div>
+          <button onClick={() => router.push("/signin")} className="h-32 rounded-full bg-gray-900 px-16 text-14 font-600 leading-loose text-white-white pc:h-40 pc:text-18">
+            로그인 하기
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const FollowArtistHero = () => {
+  const router = useRouter();
+
+  return (
+    <div className="w-full px-20 pc:px-40">
+      <div className="flex-center relative h-160 overflow-hidden rounded-lg border border-main-pink-50 pc:h-232">
+        <Image src="/image/pink-hero.png" fill sizes="100%" className="object-cover" alt="배너이미지" />
+        <div className="flex-center z-heart w-full flex-col gap-16">
+          <p className="text-center text-18 font-600 text-main-pink-500 pc:text-20">
+            좋아하는 아티스트를 설정하여
+            <br />
+            원하는 행사만 더 빠르게 확인해 보세요!
+          </p>
+          <button
+            onClick={() => router.push("/setting/favorite")}
+            className="h-32 rounded-full bg-main-pink-500 px-16 text-14 font-600 leading-loose text-white-white pc:h-40 pc:text-18"
+          >
+            설정하러 가기
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const NoNewCard = () => {
+  return (
+    <div className="w-full px-20 pc:px-40">
+      <div className="flex-center relative h-160 overflow-hidden rounded-lg border border-gray-200 pc:h-232">
+        <Image src="/image/gray-hero.png" fill sizes="100%" className="object-cover" alt="배너이미지" />
+        <div className="flex-center z-heart w-full flex-col gap-16">
+          <p className="text-center text-18 font-600 text-gray-500 pc:text-20">팔로우한 아티스트의 예정된 새 행사가 없습니다.</p>
         </div>
       </div>
     </div>
