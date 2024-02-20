4749a60a6180aaefe07095e6eed7a894768d09d7
안희원 <hw7666@naver.com>
2024-02-20 15:32:24 +0900

    ✨ feat: edit 페이지 로딩중 처리

--- app/(route)/event/[eventId]/edit/page.tsx
+++ app/(route)/event/[eventId]/edit/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import LoadingDot from "@/(route)/(bottom-nav)/signin/_components/LoadingDot";
 import { PostType } from "@/(route)/post/page";
 import { instance } from "app/_api/api";
 import { format } from "date-fns";
@@ -56,10 +57,14 @@ const Edit = () => {
     <PinkLayout size="narrow">
       <MobileHeader />
       <div className="p-20 pb-120 text-16 pc:p-0">
-        {init && (
+        {init ? (
           <GenericFormProvider formOptions={{ mode: "onChange", defaultValues: INITIAL_DATA, shouldFocusError: true }}>
             <EditContent />
           </GenericFormProvider>
+        ) : (
+          <div className="flex h-[10vh] w-full items-center justify-center">
+            <LoadingDot />
+          </div>
         )}
       </div>
     </PinkLayout>
