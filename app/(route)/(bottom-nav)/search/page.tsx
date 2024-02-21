32856f6c3597eb71e8c3350ae511467c51ecdefb
임건우 <geonulim@gmail.com>
2024-02-22 02:08:54 +0900

    🩹 chore: 헤더 높이 수정

--- app/(route)/(bottom-nav)/search/page.tsx
+++ app/(route)/(bottom-nav)/search/page.tsx
@@ -177,7 +177,7 @@ const SearchPage = () => {
 
   const handleScroll = useCallback(() => {
     const moving = window.scrollY;
-    setVisible(position < 100 || position > moving);
+    setVisible(position < 110 || position > moving);
     setPosition(moving);
   }, [position]);
 
@@ -190,7 +190,7 @@ const SearchPage = () => {
       <DottedLayout size="wide">
         <main className="relative w-full px-20 pb-84 pt-160 pc:p-0 pc:pb-84">
           <section className="fixed left-0 right-0 top-0 z-nav flex w-full flex-col bg-white-black text-14 text-gray-500 shadow-top pc:static pc:shadow-none">
-            <div className="bg-white-black px-20 pb-8 pt-40 pc:px-0 pc:pb-20 pc:pt-[7rem]">
+            <div className="bg-white-black px-20 pb-8 pt-32 pc:px-0 pc:pb-20 pc:pt-[7rem]">
               <SearchInput keyword={keyword} setKeyword={setKeyword} initialKeyword={initialKeyword} placeholder="최애의 이름으로 행사를 찾아보세요!" />
             </div>
             <div className={`animate-fadeIn px-20 pb-8 pc:p-0 ${visible ? "block" : "hidden pc:block"}`}>
