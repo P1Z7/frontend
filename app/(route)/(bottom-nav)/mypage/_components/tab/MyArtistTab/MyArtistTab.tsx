75c4137fb330811a1a86acfd79452e09ab5206d8
김다은 <kde98892@gmail.com>
2024-02-27 10:52:29 +0900

    ✨ feat: /mypage 아티스트 탭 하단에 이벤트 리스트 추가 (디자인 시안 안나옴)

---
+++ app/(route)/(bottom-nav)/mypage/_components/tab/MyArtistTab/MyArtistTab.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { FollowArtistHero } from "@/(route)/_components/carousel/FavArtistEventsCarousel";
+import { useQuery } from "@tanstack/react-query";
+import { instance } from "@/api/api";
+import { ArtistType } from "@/types/index";
+import NoContent from "../../NoContent";
+import MyArtistEvent from "./MyArtistEvent";
+import MyArtists from "./MyArtists";
+
+interface Props {
+  userId: string;
+}
+
+const MyArtistTab = ({ userId }: Props) => {
+  const { data: myArtistsData, isSuccess } = useQuery<ArtistType[]>({
+    queryKey: ["myArtist"],
+    queryFn: async () => {
+      return instance.get(`/users/${userId}/artists`);
+    },
+  });
+
+  if (!isSuccess) return;
+  return (
+    <>
+      {!myArtistsData?.length ? (
+        <div className="px-20 py-24 pb-88 pc:gap-24 pc:p-32 pc:pb-16">
+          <FollowArtistHero />
+        </div>
+      ) : (
+        <div className="flex w-full flex-col items-start gap-80 px-20 py-24 pb-88 pc:gap-60 pc:p-32 pc:pb-16">
+          <MyArtists data={myArtistsData} />
+          <MyArtistEvent userId={userId} />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default MyArtistTab;
---
+++ app/(route)/(bottom-nav)/mypage/_components/tab/MyArtistTab/MyArtists.tsx
@@ -0,0 +1,40 @@
+import { useRouter } from "next/navigation";
+import ArtistCard from "@/components/ArtistCard";
+import { ArtistType } from "@/types/index";
+
+const MyArtists = ({ data }: { data: ArtistType[] }) => {
+  const router = useRouter();
+
+  return (
+    <div className="flex w-full flex-col gap-16">
+      <div className="flex w-full items-center justify-between">
+        <h1 className="text-16 font-600">내 아티스트</h1>
+        <button className="pl- text-14 font-500 text-blue" onClick={() => router.push("/setting/my-artist")}>
+          팔로우 아티스트 수정하기
+        </button>
+      </div>
+      <div className="flex w-full flex-col items-center">
+        <div className="flex w-full flex-wrap justify-start gap-20 pc:gap-32">
+          {data.map((artist) => {
+            if (!artist) {
+              return null;
+            }
+            return (
+              <ArtistCard
+                key={artist.id}
+                profileImage={artist.image}
+                onClick={() => {
+                  router.push(`/search?keyword=${artist.name}`);
+                }}
+              >
+                {artist.name}
+              </ArtistCard>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MyArtists;
