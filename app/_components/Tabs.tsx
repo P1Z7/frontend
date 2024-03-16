67acb7a42798e91ef72e3a0f336a6282af59c69c
임건우 <geonulim@gmail.com>
2024-03-16 17:13:53 +0900

    ⚡️ fix: 탭 top offset 수정

--- app/_components/Tabs.tsx
+++ app/_components/Tabs.tsx
@@ -3,25 +3,20 @@ import { ReactNode } from "react";
 import TabButton from "@/components/TabButton";
 import { Tab } from "@/store/tab/cookies";
 
-const TOP_OFFSET = {
-  mypage: "top-0",
-  event: "top-60",
-};
-
 interface Props {
   children: ReactNode[];
   names: readonly string[];
-  topOffset?: "mypage" | "event";
+  page?: "mypage" | "event";
   eventId?: string;
   isNarrow?: boolean;
 }
 
-const Tabs = ({ children, names, topOffset = "mypage", eventId, isNarrow: narrow = false }: Props) => {
-  const tab: Tab = JSON.parse(cookies().get(eventId ? topOffset + "/" + eventId : topOffset)?.value ?? `{ "name": "${names[0]}", "index": "0"}`);
+const Tabs = ({ children, names, page = "mypage", eventId, isNarrow: narrow = false }: Props) => {
+  const tab: Tab = JSON.parse(cookies().get(eventId ? page + "/" + eventId : page)?.value ?? `{ "name": "${names[0]}", "index": "0"}`);
 
   return (
     <section className="h-full w-full">
-      <div className={`sticky z-popup flex h-44 w-full items-center border-b border-gray-50 bg-white-black px-20 pc:h-56 ${TOP_OFFSET[topOffset]} ${narrow && "pc:static"}`}>
+      <div className={`sticky top-72 z-popup flex h-44 w-full items-center border-b border-gray-50 bg-white-black px-20 pc:h-56 ${narrow && "pc:static"}`}>
         {names.map((name, index) => (
           <TabButton key={name} tab={{ name, index }} selected={name === tab.name} narrow={narrow}>
             {name}
