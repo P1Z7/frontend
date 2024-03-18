4cd2652f972269a2bf06d5c8488cbb140cb929d6
임건우 <geonulim@gmail.com>
2024-03-18 17:05:26 +0900

    ⚡️ fix: 헤더 검색바 검색 후 값 초기화

--- app/_components/input/SearchInput.tsx
+++ app/_components/input/SearchInput.tsx
@@ -1,4 +1,4 @@
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 import { Dispatch, SetStateAction, useEffect } from "react";
 import { KeyboardEvent } from "react";
 import { useForm } from "react-hook-form";
@@ -10,11 +10,9 @@ interface Props {
   setKeyword: Dispatch<SetStateAction<string>>;
   initialKeyword?: string;
   placeholder?: string;
-  size?: "lg" | "sm";
-  href?: string;
 }
 
-const SearchInput = ({ keyword, setKeyword, initialKeyword, href, placeholder = "검색어를 입력하세요.", size = "lg" }: Props) => {
+const SearchInput = ({ keyword, setKeyword, initialKeyword, placeholder = "검색어를 입력하세요." }: Props) => {
   const { register, getValues, setValue, watch } = useForm({
     defaultValues: {
       search: initialKeyword,
@@ -29,13 +27,8 @@ const SearchInput = ({ keyword, setKeyword, initialKeyword, href, placeholder = 
     }
   };
 
-  const router = useRouter();
   const handleSearchClick = () => {
     const newKeyword = getValues("search") ?? "";
-    if (!newKeyword && href) {
-      router.push(href);
-    }
-
     setKeyword(newKeyword);
   };
 
@@ -60,17 +53,17 @@ const SearchInput = ({ keyword, setKeyword, initialKeyword, href, placeholder = 
     <div className="relative">
       <input
         autoFocus={curPath === "/post" || curPath === "/edit"}
-        className={`h-44 w-full rounded-full bg-gray-50 px-16 py-12 pr-68 text-16 text-black-white placeholder:text-gray-400 focus:outline-none ${size === "lg" ? "pc:h-52 pc:border pc:border-gray-100 pc:bg-white-black" : ""} pc:px-20 pc:pr-76`}
+        className="h-44 w-full rounded-full bg-gray-50 px-16 py-12 pr-68 text-16 text-black-white placeholder:text-gray-400 focus:outline-none pc:h-52 pc:border pc:border-gray-100 pc:bg-white-black pc:px-20 pc:pr-76"
         placeholder={placeholder}
         {...register("search")}
         onKeyDown={handleSearchEnter}
         autoComplete="off"
       />
-      <button className={`flex-center absolute right-12 top-4 h-36 w-36 rounded-full bg-gray-50 ${size === "lg" ? "pc:top-8" : ""}`} type="button" onClick={handleSearchClick}>
+      <button className="flex-center absolute right-12 top-4 h-36 w-36 rounded-full bg-gray-50 pc:top-8" type="button" onClick={handleSearchClick}>
         <SearchIcon width="20" height="20" stroke="#494F5A" />
       </button>
       {search && (
-        <button className={`absolute right-48 top-[1.4rem] pc:right-56 ${size === "lg" ? "pc:top-[1.8rem]" : ""}`} type="button" onClick={handleCloseClick}>
+        <button className="absolute right-48 top-[1.4rem] pc:right-56 pc:top-[1.8rem]" type="button" onClick={handleCloseClick}>
           <CloseIcon stroke="#A0A5B1" width="16" height="16" />
         </button>
       )}
